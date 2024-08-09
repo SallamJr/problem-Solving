70bcf30504255e37f2086a4921dd071171711ee0
Ahmed Sallam <112440167+SallamJr@users.noreply.github.com>
2024-08-09 16:59:21 +0300

    [169] [Time Beats: 99.66%] [Memory Beats: 41.03%] - LeetPush

---
+++ 169-Majority-Element.dart
@@ -0,0 +1,15 @@
+class Solution {
+  int majorityElement(List<int> nums) {
+   int count = 0;
+  int? candidate;
+
+  for (int num in nums) {
+    if (count == 0) {
+      candidate = num;
+    }
+    count += (num == candidate) ? 1 : -1;
+  }
+
+  return candidate!;  
+  }
+}

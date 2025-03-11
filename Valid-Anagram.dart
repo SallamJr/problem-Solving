cf2c055705d3e455e839505d0517c4423273f627
Ahmed Sallam <112440167+SallamJr@users.noreply.github.com>
2025-03-11 13:36:12 +0200

    [242] [Time Beats: 23.48%] [Memory Beats: 34.82%] - LeetPush

---
+++ Valid-Anagram.dart
@@ -0,0 +1,10 @@
+class Solution {
+  bool isAnagram(String s, String t) {
+    if (s.length != t.length) return false;
+  
+  List<String> sortedS = s.split('')..sort();
+  List<String> sortedT = t.split('')..sort();
+
+  return sortedS.join() == sortedT.join();
+  }
+}

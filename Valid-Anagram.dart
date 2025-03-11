738712f1ccd2b03553efe9a7f42f0cf27c2cb6fd
Ahmed Sallam <112440167+SallamJr@users.noreply.github.com>
2025-03-11 13:37:40 +0200

    [242] [Time Beats: 23.48%] [Memory Beats: 34.82%] - LeetPush

--- Valid-Anagram.dart
+++ Valid-Anagram.dart
@@ -1,5 +1,7 @@
 class Solution {
   bool isAnagram(String s, String t) {
+
+    ///An anagram is a word or phrase formed by rearranging the letters of another word or phrase, using all the original letters exactly once. For example, the word \listen\ can be rearranged to form \silent.
     if (s.length != t.length) return false;
   
   List<String> sortedS = s.split('')..sort();
